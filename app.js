bda2de2c5440530f3e8de8be58cd5074b0059016
Joel Trost <joelt@zillow.com>
2014-06-18 16:39:51 -0700

    changed msg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function *(){
-  this.body = 'Jason, You\'re an Idiot \n';
+  this.body = 'Hello World \n';
 });
 
 if (!module.parent) app.listen(8080);
